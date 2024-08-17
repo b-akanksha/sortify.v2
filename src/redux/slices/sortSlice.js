7f7d7e59be429609542d4464fc792bb00e77318d
Akanksha C <akankshac@Akankshas-MacBook-Air.local>
2024-08-17 12:02:08 +0530

    feat: project skeleton

---
+++ src/redux/slices/sortSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  alogirthms: [
+    {id: 'bubbleSort', name: 'Bubble Sort'},
+    {id: 'selectionSort', name: 'Selection Sort'}
+  ],
+  selected: null
+}
+
+export const sortSlice = createSlice({
+  name: 'sort',
+  initialState,
+  reducers: {
+    setAlgorithm: (state, { payload }) => {
+      state.selected = payload;
+    },
+    resetAlgorithm: (state) => {
+        state.selected = null
+    }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setAlgorithm, resetAlgorithm } = sortSlice.actions
+
+export default sortSlice.reducer
